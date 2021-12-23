c98e1c3683507fb6c4b7b994051fdd699e53303b
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 01:09:30 +0300

    Add quality degrades twice as fast test

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -12,6 +12,14 @@ class GildedRoseTest  extends AnyFlatSpec with Matchers {
     app.items(0).name should equal ("foo")
   }
 
+  "quality of an item" should "degrades twice as fast" in {
+    val items = Array[Item](new Item("foo", 0, 10))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 8
+    app.items(0).quality should equal(expected)
+  }
+
   "quality of an item" should "be positive" in {
     val items = Array[Item](new Item("foo", 5, 0))
     val app = new GildedRose(items)
