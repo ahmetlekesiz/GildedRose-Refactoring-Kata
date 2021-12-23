17c59155e10267a1e3ea8719f600efc3e3fb4752
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 01:10:08 +0300

    Add sellIn and quality tests

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -51,4 +51,44 @@ class GildedRoseTest  extends AnyFlatSpec with Matchers {
     val expected = 50
     app.items(0).quality should equal(expected)
   }
+
+  "sellIn of Sulfuras, Hand of Ragnaros" should "not decrease" in {
+    val items = Array[Item](new Item("Sulfuras, Hand of Ragnaros", 5, 50))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 5
+    app.items(0).sellIn should equal(expected)
+  }
+
+  "quality of Sulfuras, Hand of Ragnaros" should "not change" in {
+    val items = Array[Item](new Item("Sulfuras, Hand of Ragnaros", 5, 50))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 50
+    app.items(0).quality should equal(expected)
+  }
+
+  "quality of Backstage passes" should "increase by 2 when there are 10 days or less" in {
+    val items = Array[Item](new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 22
+    app.items(0).quality should equal(expected)
+  }
+
+  "quality of Backstage passes" should "increase by 3 when there are 5 days or less" in {
+    val items = Array[Item](new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 23
+    app.items(0).quality should equal(expected)
+  }
+
+  "quality of Backstage passes" should "drops to 0 after the concert" in {
+    val items = Array[Item](new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 0
+    app.items(0).quality should equal(expected)
+  }
 }
