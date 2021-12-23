18a846bcdf25dad161f26709ca5b8b9509100cc5
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-23 23:54:39 +0300

    Add quality of Aged Brie increase test when it gets older

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -5,18 +5,26 @@ import org.scalatest.matchers.should.Matchers
 
 class GildedRoseTest  extends AnyFlatSpec with Matchers {
 
-  val items = Array[Item](new Item("foo", 5, 0))
-
   it should "foo" in {
+    val items = Array[Item](new Item("foo", 5, 0))
     val app = new GildedRose(items)
     app.updateQuality()
     app.items(0).name should equal ("foo")
   }
 
   "quality of an item" should "be positive" in {
+    val items = Array[Item](new Item("foo", 5, 0))
     val app = new GildedRose(items)
     app.updateQuality()
     val expected = 0
+    app.items(0).quality should equal(expected)
+  }
+
+  "quality of Aged Brie" should "increase when it gets older" in {
+    val items = Array[Item](new Item("Aged Brie", 5, 0))
+    val app = new GildedRose(items)
+    app.updateQuality()
+    val expected = 1
     app.items(0).quality should equal(expected)
   }
 }
