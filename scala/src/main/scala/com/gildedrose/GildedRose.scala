949dd3e4dd100de746410ce8186c92aeaabfe93e
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 05:10:39 +0300

    Refactor Aged Brie Update Quality

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -13,12 +13,10 @@ class GildedRose(val items: Array[Item]) {
   }
 
   def AgedBrieUpdateQuality(item: Item): Unit = {
+    item.sellIn = item.sellIn - 1
     if (item.quality < 50) {
       item.quality = item.quality + 1
-    }
-    item.sellIn = item.sellIn - 1
-    if (item.sellIn < 0) {
-      if (item.quality < 50) {
+      if (item.sellIn < 0) {
         item.quality = item.quality + 1
       }
     }
