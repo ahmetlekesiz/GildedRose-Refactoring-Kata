9907b6815377a78b2ab44b901cb9766b47185abd
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 04:36:54 +0300

    Add special update quality function for Backstage Passes

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -75,9 +75,30 @@ class GildedRose(val items: Array[Item]) {
 
   def SulfurasUpdateQuality: Unit = {}
 
+  def BackstagePasses(item: Item): Unit = {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+      if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      }
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      }
+    }
+    item.sellIn = item.sellIn - 1
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality
+    }
+  }
+
   def callSpecialFunction(item: Item): Unit = item.name match {
     case ItemName.AgedBrie  => AgedBrieUpdateQuality(item)
     case ItemName.Sulfuras  => SulfurasUpdateQuality
+    case ItemName.BackstagePasses => BackstagePasses(item)
     case _                  => updateQuailtyDefault(item)
   }
 
