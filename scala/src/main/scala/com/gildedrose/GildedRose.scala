91e9891975cb02a8df09fbcd2d0b442d997460e7
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 04:31:27 +0300

    Add special update quality function for Sulfuras

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,64 +1,64 @@
 package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
+  
+  def updateQuailtyDefault(item: Item): Unit = {
+    if (!item.name.equals("Aged Brie")
+      && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (item.quality > 0) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (item.name.equals("Conjured Mana Cake")) {
+            item.quality = item.quality - 2
+          } else {
+            item.quality = item.quality - 1
+          }
+        }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
 
-
-  def updateQuailtyDefault(item: Item): Unit ={
-      if (!item.name.equals("Aged Brie")
-        && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (item.quality > 0) {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            if (item.name.equals("Conjured Mana Cake")){
-              item.quality = item.quality - 2
-            }else{
-              item.quality = item.quality - 1
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
             }
           }
+
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1
+            }
+          }
+        }
+      }
+    }
+
+    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+      item.sellIn = item.sellIn - 1
+    }
+
+    if (item.sellIn < 0) {
+      if (!item.name.equals("Aged Brie")) {
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (item.name.equals("Conjured Mana Cake")) {
+                item.quality = item.quality - 2
+              } else {
+                item.quality = item.quality - 1
+              }
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1
-
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
         }
       }
-
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        item.sellIn = item.sellIn - 1
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
-          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.name.equals("Conjured Mana Cake")){
-                  item.quality = item.quality - 2
-                }else{
-                  item.quality = item.quality - 1
-                }              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
-      }
+    }
   }
 
   def AgedBrieUpdateQuality(item: Item): Unit = {
@@ -68,13 +68,16 @@ class GildedRose(val items: Array[Item]) {
     item.sellIn = item.sellIn - 1
     if (item.sellIn < 0) {
       if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+        item.quality = item.quality + 1
+      }
     }
   }
 
+  def SulfurasUpdateQuality: Unit = {}
+
   def callSpecialFunction(item: Item): Unit = item.name match {
     case ItemName.AgedBrie  => AgedBrieUpdateQuality(item)
+    case ItemName.Sulfuras  => SulfurasUpdateQuality
     case _                  => updateQuailtyDefault(item)
   }
 
