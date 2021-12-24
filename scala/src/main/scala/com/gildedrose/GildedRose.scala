a851a4435caa97b615aca32c9f333eb66061bce0
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 05:13:02 +0300

    Refactor Conjured Update Quality

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,7 +1,7 @@
 package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
-  
+
   def updateQualityDefault(item: Item): Unit = {
     item.sellIn = item.sellIn - 1
     if (item.quality > 0) {
@@ -43,12 +43,10 @@ class GildedRose(val items: Array[Item]) {
   }
 
   def ConjuredUpdateQuality(item: Item): Unit = {
+    item.sellIn = item.sellIn - 1
     if (item.quality > 0) {
       item.quality = item.quality - 2
-    }
-    item.sellIn = item.sellIn - 1
-    if (item.sellIn < 0) {
-      if (item.quality > 0) {
+      if (item.sellIn < 0) {
         item.quality = item.quality - 2
       }
     }
