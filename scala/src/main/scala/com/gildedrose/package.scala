8280154790e575c6640ba41e1e0474b38b76d453
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 04:11:12 +0300

    Add item names

---
+++ scala/src/main/scala/com/gildedrose/package.scala
@@ -0,0 +1,10 @@
+package com
+
+package object gildedrose {
+  object ItemName {
+    val AgedBrie = "Aged Brie"
+    val Sulfuras = "Sulfuras, Hand of Ragnaros"
+    val BackstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+    val Conjured = "Conjured Mana Cake"
+  }
+}
