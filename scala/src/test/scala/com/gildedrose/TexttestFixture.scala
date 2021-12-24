d2a4c8a286de42e4ea9581f76b5ea18a0dacceb5
ahmetlekesiz <ahmet.lekesiz@trendyol.com>
2021-12-24 05:58:29 +0300

    Update number of days and remove additional item in text test fixture

--- scala/src/test/scala/com/gildedrose/TexttestFixture.scala
+++ scala/src/test/scala/com/gildedrose/TexttestFixture.scala
@@ -8,7 +8,6 @@ object TexttestFixture {
       new Item("Elixir of the Mongoose", 5, 7),
       new Item("Sulfuras, Hand of Ragnaros", 0, 80),
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-      new Item("Sulfuras, Hand of Ragnaros", 2, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
@@ -16,7 +15,7 @@ object TexttestFixture {
       new Item("Conjured Mana Cake", 3, 6)
     )
     val app = new GildedRose(items)
-    val days = if (args.length > 0) args(0).toInt + 1 else 30
+    val days = if (args.length > 0) args(0).toInt + 1 else 31
     for (i <- 0 until days) {
       System.out.println("-------- day " + i + " --------")
       System.out.println("name, sellIn, quality")
